6325b0d05a54d5f34e82aed4b47e6d9fb44e2bf5
gautamswami <gautams5974@gmail.com>
2022-03-08 15:45:26 +0530

    v.0.01

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function displayRadioValue() {
         var ele = document.getElementsByName('fruit');
          var ele1 =  document.getElementsByName('fruit1');
+         var ele2 =  document.getElementsByName('fruit2');
+         
         for(i = 0; i < ele.length; i++) {
             if(ele[i].checked  )
             document.getElementById("result").innerHTML
@@ -10,5 +12,10 @@ function displayRadioValue() {
             if(ele1[i].checked  )
             document.getElementById("result1").innerHTML
                     = "SELECTED FROM BOX 2: "+ele1[i].value ;
+        }
+        for(i = 0; i < ele2.length; i++) {
+            if(ele2[i].checked  )
+            document.getElementById("result2").innerHTML
+                    = "SELECTED FROM BOX 3: "+ele2[i].value ;
         }
     }
